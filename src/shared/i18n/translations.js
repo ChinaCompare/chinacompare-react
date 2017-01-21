93707ccb518cd3cc3951d38da5cd78b60b39ac80
user <user@user>
2017-01-21 11:03:27 +0100

    add translations

---
+++ src/shared/i18n/translations.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line
+var merge = require('deepmerge');
+
+// translate:start
+const transca = require('../components/App/translate/locale-ca.json');
+const transen = require('../components/App/translate/locale-en.json');
+const transes = require('../components/App/translate/locale-es.json');
+export const translate = {
+  ca: transca,
+  en: transen,
+  es: transes
+};
+// translate:end
+
+// eslint-disable-next-line
+export default translate;
